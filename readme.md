fa5caca555a7d82fbcaf5f475245f4a0952c3b14
samian96 <sam.iannazzo@yahoo.com>
2025-11-02 12:14:16 -0800

    updated readme / added let r / let res/ consolelog

--- readme.md
+++ readme.md
@@ -5,3 +5,5 @@ Created Readme file, added Author and purpose to file.
 - Update 2: 
 added a function search string to count how many letter 't' where in the word constantinople
 used const and while loop to determine outcome
+- Update 3:
+added function to output the circumference of a circle.
--- script.js
+++ script.js
@@ -6,7 +6,9 @@
 // 2. Write a function that takes the radius of a circle and outputs it’s approximate circumference (the formula for circumference is 2 *pi * radius, you may use 3.14 for pi).
 // Example Output:
 // console.log(calculateCircumference(5)); // will output 31.4
-
+let r = 5; 
+let res = 5 * 3.14 * r; 
+console.log(`${res.toFixed(5)}`);
 // 3. Write a function searchString that takes a character and then returns how many times it appears in another string argument.
 // Example Output:
 // console.log(searchString(“s”, “mississippi”)); // will output 4
@@ -14,9 +16,9 @@
 const word1 = "constantinople";
 const letter1 = "t";
 let count = 0;
-let pos = word1.indexOf(letter1);
-while (pos !== -1) {
+let position = word1.indexOf(letter1);
+while (position !== -1) {
     count++;
-    pos = word1.indexOf(letter1, pos + letter1.length);
+    position = word1.indexOf(letter1, position + letter1.length);
 }
 console.log(count);
